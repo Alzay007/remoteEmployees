43ae3d3d95689eefcedce6e93678f302775acc19
Andrew Zaitsev <alzay007@gmail.com>
2023-03-27 14:31:58 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This project implements this [Figma design](https://www.figma.com/file/7qwsWggv9BAxMi2VPhBuPr/Air-(formerly-Dia)?node-id=9138%3A35)
+
+The page adapted to the following screens:
+- Large screens 2560px
+- The design 1600px
+- Desktop 1280px
+- Small desktop 1024px
+- Tablet 640px
+- Mobile (> 320px)
+
+Layout created pixel perfect using HTML and SCSS according to BEM methodology.
+
+[WORKING DEMO](https://alzay007.github.io/dia-landing/)
---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+// import Swiper, { Navigation } from 'swiper';
+// import 'swiper/swiper-bundle.min.css';
+
+// // eslint-disable-next-line no-unused-vars
+// const swiper = new Swiper('.swiper', {
+//   modules: [Navigation],
+
+//   navigation: {
+//     nextEl: '.swiper-arrow-next',
+//     prevEl: '.swiper-arrow-prev',
+//   },
+// });
+
+// const form = document.querySelector('.form');
+
+// form.addEventListener('submit', function (event) {
+//   event.preventDefault();
+
+//   form.reset();
+// });
+
+// window.addEventListener('hashchange', () => {
+//   if (window.location.hash === '#menu') {
+//     document.body.classList.add('page__body--with-menu');
+//   } else {
+//     document.body.classList.remove('page__body--with-menu');
+//   }
+// });
