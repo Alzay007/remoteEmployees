24d01bf95cd2d52f1ce3dd4b6262780919b8c18f
Andrew Zaitsev <alzay007@gmail.com>
2023-03-29 14:47:33 +0300

    change readme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,53 +1,59 @@
+/* eslint-disable no-unused-vars */
+'use strict';
+
+// eslint-disable-next-line no-undef
 const swiperOne = new Swiper('.swiper-header', {
-    direction: 'horizontal',
-    loop: true,
+  direction: 'horizontal',
+  loop: true,
 
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-    },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
 
-    navigation: {
-        nextEl: '.button-next',
-        prevEl: '.button-prev',
-    },
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
 });
 
+// eslint-disable-next-line no-undef
 const swiperTwo = new Swiper('.swiper-featured', {
-    direction: 'horizontal',
-    slidesPerView: 3,
-    slidesPerColumn: 2,
-    spaceBetween: 50,
-    loop: true,
+  direction: 'horizontal',
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  spaceBetween: 50,
+  loop: true,
 
-    autoplay: {
-        delay: 5000,
-    },
+  autoplay: {
+    delay: 5000,
+  },
 
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-    },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
 });
 
+// eslint-disable-next-line no-undef
 const swiperThree = new Swiper('.swiper-editors', {
-    slidesPerView: 1,
-    slidesPerColumnFill: 'column',
+  slidesPerView: 1,
+  slidesPerColumnFill: 'column',
 
-    grid: {
-        rows: 2,
-        fill: "row",
-    },
+  grid: {
+    rows: 2,
+    fill: 'row',
+  },
 
-    spaceBetween: 50,
+  spaceBetween: 50,
 
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-    },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
 
-    navigation: {
-        nextEl: '.button-next',
-        prevEl: '.button-prev',
-    },
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
 });
